3dec20e0229664a8f6307ccd95ea6fb8cc24e56a
borisdovecer <boris.dovecer@gmail.com>
2024-03-02 14:43:52 +0100

    metadata

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-const manifestUrl = "";
+const manifestUrl = "https://raw.githubusercontent.com/TONFundsTeam/TONFunds-Frontend/main/public/appMetadata.json";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
